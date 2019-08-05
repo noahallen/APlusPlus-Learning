84e3dbb64819730356f86cf484e92ef260d9b32a
NoahAllen <noahcallen@hotmail.com>
2019-08-05 13:50:11 -0700

    First commit! Has company landing site

---
+++ public/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAweuAMELhUKwoT1HTxH8sDlhPKMUDKIEw",
+    authDomain: "study-abroad-tutor-app.firebaseapp.com",
+    databaseURL: "https://study-abroad-tutor-app.firebaseio.com",
+    projectId: "study-abroad-tutor-app",
+    storageBucket: "",
+    messagingSenderId: "951054056709",
+    appId: "1:951054056709:web:b48837f0c613fca2"
+  };
