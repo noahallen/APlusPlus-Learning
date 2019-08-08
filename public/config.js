00def0fe0367c790bab4ec1ddd57e26cb5de3a64
NoahAllen <noahcallen@hotmail.com>
2019-08-08 12:39:27 -0700

    Added Google Auth Button and Login

--- public/config.js
+++ public/config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
     authDomain: "study-abroad-tutor-app.firebaseapp.com",
     databaseURL: "https://study-abroad-tutor-app.firebaseio.com",
     projectId: "study-abroad-tutor-app",
-    storageBucket: "",
+    storageBucket: "study-abroad-tutor-app.appspot.com",
     messagingSenderId: "951054056709",
     appId: "1:951054056709:web:b48837f0c613fca2"
   };
--- public/landing.js
+++ public/landing.js
@@ -1,1 +1,28 @@
 //Landing Page Javascript
+
+function initialize(){
+    firebase.initializeApp(firebaseConfig);
+}
+
+function login(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+}
+
